fd5b2df6b436cbe78957659c116a764018eb1864
ravinduf <ravinduf@rocketmail.com>
2021-05-01 10:43:16 +0530

    updated
    added todo model basic table to show tasks

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# /// -> 3*/ relative path
+# //// -> 4*/ absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(200), nullable=False)
+  date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    return '<Task %r>' % self.id
+
 
 @app.route('/')
 def index():
-    return "Hello world"
+  return render_template('index.html')
 
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+  app.run(debug=True)
